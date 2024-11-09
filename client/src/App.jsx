0157ee2641c76dddf4a2e7d11f7b7d103be49dbb
TARANSH CHELLANI <taranshchellani121@gmail.com>
2024-11-09 22:40:44 +0530

    added login signup frontend.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App ok</h1>
+    <div className="">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
